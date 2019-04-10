b1651aa5b2bfe785b35dfd84d17d9e69ca2c9398
Elma Rahman <elmarahman635@gmail.com>
2019-04-10 17:32:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,7 @@ def loop_iterator(number_of_times)
   loop do
 puts "Welcome to Flatiron School's Web Development Course!"
 loop_iterator == 7
-    break if loop_iterator == 8
+    break if loop_iterator == 7
   end
 
   # Code your solution here using the "loop" keyword to puts out the below phrase
